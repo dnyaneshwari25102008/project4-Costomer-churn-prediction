6b558d7f4f7160e53828805619906f98588c8f7f
dnyaneshwari25102008 <bhosalednyaneshwari36@gmail.com>
2026-07-05 23:49:48 +0530

    Add files via upload

---
+++ xyz.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import sklearn
+import streamlit as st
+import matplotlib as plt
+import imblearn 
+
+print(np.__version__)
+print(pd.__version__)
+print(sklearn.__version__)
+print(st.__version__)
+print(plt.__version__)
+print(imblearn.__version__)
